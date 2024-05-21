1d850fb8013ba4f7c07c8a996683b3f6fdb4d002
joao.paulo@credencial.net.br <joao.paulo@credencial.net.br>
2024-05-21 22:40:20 +0000

    fix: Nome tabela metadados

--- src/02.bronze/compras_contratos/ingestao.py
+++ src/02.bronze/compras_contratos/ingestao.py
@@ -1,14 +1,20 @@
 # Databricks notebook source
+from pyspark.sql import SparkSession
+
+import os
 import sys
-sys.path.insert(0, '..\..\lib')
+sys.path.insert(0, "../../../lib")
+
 from ingestors import IngestionBronze
 
 # COMMAND ----------
 
+spark = SparkSession.builder.appName("IngestionBronze").getOrCreate()
+
 area = 'compras_contratos'
 api_url = f'https://dados.mg.gov.br/api/3/action/package_show?id={area}'
-output_dir = f"/dbfs/mnt/datalake/transparencia-mg/{area}/data"
-metadata_table = f'bronze.transparencia-mg.metadata_{area}'
+output_dir = f"/dbfs/mnt/datalake/transparencia-mg/bronze/{area}/data"
+metadata_table = f'bronze.transparencia_mg.metadata_{area}'
 
-bronze = IngestionBronze(api_url, metadata_table)
+bronze = IngestionBronze(api_url, spark, metadata_table)
 bronze.get_and_save(output_dir)
