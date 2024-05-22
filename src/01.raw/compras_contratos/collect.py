5883cbb6bf28ca20269dff8d7505ae1965b5ccaf
Jptudobem <joao.paulo@credencial.net.br>
2024-05-22 09:37:17 -0300

    Coleta de dados camada raw

---
+++ src/01.raw/compras_contratos/collect.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+from pyspark.sql import SparkSession
+
+import os
+import sys
+sys.path.insert(0, "../../../lib")
+
+from collector import Raw
+
+# COMMAND ----------
+
+spark = SparkSession.builder.appName("Raw").getOrCreate()
+
+area = 'compras_contratos'
+api_url = f'https://dados.mg.gov.br/api/3/action/package_show?id={area}'
+output_dir = f"/dbfs/mnt/datalake/transparencia-mg/raw/{area}/data"
+metadata_table = f'raw.transparencia_mg.metadata_{area}'
+
+raw = Raw(api_url, spark, metadata_table)
+raw.get_and_save(output_dir)
